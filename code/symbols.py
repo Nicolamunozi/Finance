2a6be6547af07e61f7d0c5531bf9922b6b7dd190
Nicolas <nicolamunozi@gmail.com>
2022-11-07 11:05:22 -0300

    adding if main to symbols

--- code/symbols.py
+++ code/symbols.py
@@ -3,46 +3,55 @@ from yahoo_fin import stock_info as si
 
 # this is comming from: https://medium.com/gitconnected/how-to-get-all-stock-symbols-a73925c16a1b
 
-#------------Main Stocks exchanges in the US ---------------#
 
-df1 = pd.DataFrame(si.tickers_sp500())
-df2 = pd.DataFrame(si.tickers_nasdaq())
-df3 = pd.DataFrame(si.tickers_dow())
-df4 = pd.DataFrame(si.tickers_other())
+def main():
+    '''This is the main function in the article. Yet without any changes'''
 
-#------------ Each data frame to a list and then to a set ---------#
+    #------------Main Stocks exchanges in the US ---------------#
 
-sym1 = set(symbol for symbol in df1[0].values.tolist())
-sym2 = set(symbol for symbol in df2[0].values.tolist())
-sym3 = set(symbol for symbol in df3[0].values.tolist())
-sym4 = set(symbol for symbol in df4[0].values.tolist())
+    df1 = pd.DataFrame(si.tickers_sp500())
+    df2 = pd.DataFrame(si.tickers_nasdaq())
+    df3 = pd.DataFrame(si.tickers_dow())
+    df4 = pd.DataFrame(si.tickers_other())
 
-#--Merging all different sets and symbols ----------#
+    #------------ Each data frame to a list and then to a set ---------#
 
-symbols = set.union(sym1, sym2, sym3, sym4)
+    sym1 = set(symbol for symbol in df1[0].values.tolist())
+    sym2 = set(symbol for symbol in df2[0].values.tolist())
+    sym3 = set(symbol for symbol in df3[0].values.tolist())
+    sym4 = set(symbol for symbol in df4[0].values.tolist())
 
-#------- identifying junk stocks ----- #
+    #--Merging all different sets and symbols ----------#
 
-my_list = ['W', 'R', 'P', 'Q']
+    symbols = set.union(sym1, sym2, sym3, sym4)
 
-"""
-W means there are outstanding warrants. We don’t want those.
+    #------- identifying junk stocks ----- #
 
-R means there is some kind of “rights” issue. Again, not wanted.
+    my_list = ['W', 'R', 'P', 'Q']
 
-P means “First Preferred Issue”. Preferred stocks are a separate entity.
+    """
+    W means there are outstanding warrants. We don’t want those.
 
-Q means bankruptcy. We don’t want those, either.
-"""
-#-------control sets --------------#
+    R means there is some kind of “rights” issue. Again, not wanted.
 
-del_set = set()
-sav_set = set()
-for symbol in symbols:
-    if len(symbol) > 4 and symbol[-1] in my_list:
-        del_set.add(symbol)
-    else:
-        sav_set.add(symbol)
+    P means “First Preferred Issue”. Preferred stocks are a separate entity.
 
-print(f'Removed {len( del_set )} unqualified stock symbols...')
-print(f'There are {len( sav_set )} qualified stock symbols...')
+    Q means bankruptcy. We don’t want those, either.
+    """
+    #-------control sets --------------#
+
+    del_set = set()
+    sav_set = set()
+    for symbol in symbols:
+        if len(symbol) > 4 and symbol[-1] in my_list:
+            del_set.add(symbol)
+        else:
+            sav_set.add(symbol)
+
+    print(f'Removed {len( del_set )} unqualified stock symbols...')
+    print(f'There are {len( sav_set )} qualified stock symbols...')
+
+
+if __name__ == "__main__":
+
+    main()
